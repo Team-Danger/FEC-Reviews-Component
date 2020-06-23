0162842daf4221d5e5133079edb4720b510e22b0
Janet Zheng <janet.zhengg@gmail.com>
2020-06-22 18:47:42 -0700

    corrected typo

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import axios from 'axios';
 
-const App = () => (
-  <div>
-    Hello!
-  </div>
-)
+class App extends React.Component {
+  constructor(props) {
+    super(props)
 
+    this.state = {reviews: {}}
+
+    this.getFromDB = this.getFromDB.bind(this);
+  }
+
+  componentDidMount() {
+    this.getFromDB()
+  }
+
+  getFromDB() {
+    const test = (Math.floor(Math.random() * (100 - 1)) + 1).toString().padStart(3, '0');
+
+    axios.get(`/api/${test}`)
+      .then((results) => {
+        this.setState({reviews: results.data});
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  render() {
+    return (
+      <div className='Reviews'>
+        Test
+      </div>
+    )
+  }
+}
 
 export default App;
