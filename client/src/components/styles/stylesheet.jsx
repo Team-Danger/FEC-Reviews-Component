bc29cf052a971198f22db4bf21a2b42159b3ecdf
Janet Zheng <janet.zhengg@gmail.com>
2020-06-22 21:25:23 -0700

    added flex

--- client/src/components/styles/stylesheet.jsx
+++ client/src/components/styles/stylesheet.jsx
@@ -2,10 +2,12 @@ import styled from 'styled-components'
 
 const Wrapper = styled.div`
 font-family: Helvetica, Arial, Sans-Serif;
+display: flex
 `
 
 const StyledReviewSummary = styled.div`
 font-size: 2rem;
 font-weight: bolder;
+flex-direction: column;
 `
 export {Wrapper, StyledReviewSummary};
