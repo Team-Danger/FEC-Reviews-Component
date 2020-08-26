dc811fbae85237591198102a850b5c509ac62309
jzhengcode <jzhengcode@gmail.com>
2020-08-26 12:40:43 -0700

    i

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom'
 import App from './components/App';
 
-const listing = '010'
+const url = new URL(window.location.href);
+const path = url.pathname;
+const idSearch = /(\d{8})/;
+const [listingID] = path.match(idSearch);
 
-ReactDOM.render(<Router><App listing={listing} /></Router>, document.getElementById('reviews'));
+ReactDOM.render(<Router><App listing={listingID} /></Router>, document.getElementById('reviews'));
